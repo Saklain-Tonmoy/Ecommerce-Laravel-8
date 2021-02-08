879668a7add303f4ad27c4f623924a82854372a7
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-02-08 17:54:39 +0600

    commit

--- database/migrations/2021_02_08_114147_create_banners_table.php
+++ database/migrations/2021_02_08_114147_create_banners_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 class CreateBannersTable extends Migration
 {
@@ -15,6 +16,11 @@ class CreateBannersTable extends Migration
     {
         Schema::create('banners', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->string('photo');
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
             $table->timestamps();
         });
     }

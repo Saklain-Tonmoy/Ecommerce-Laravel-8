ab1e84a4822c8814aca6c7889784a1e73adcd49a
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-02-08 14:56:27 +0600

    commit

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Javascript -->
+<script src="{{asset('assets/backend/bundles/libscripts.bundle.js')}}"></script>    
+<script src="{{asset('assets/backend/bundles/vendorscripts.bundle.js')}}"></script>
+
+<script src="{{asset('assets/backend/bundles/jvectormap.bundle.js')}}"></script> <!-- JVectorMap Plugin Js -->
+<script src="{{asset('assets/backend/bundles/morrisscripts.bundle.js')}}"></script><!-- Morris Plugin Js -->
+<script src="{{asset('assets/backend/bundles/knob.bundle.js')}}"></script> <!-- Jquery Knob-->
+
+<script src="{{asset('assets/backend/bundles/mainscripts.bundle.js')}}"></script>
+<script src="{{asset('assets/backend/js/index8.js')}}"></script>
---
+++ resources/views/backend/layouts/head.blade.php
@@ -0,0 +1,20 @@
+<title>E-Commerce Dashboard</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="Lucid Bootstrap 4.1.1 Admin Template">
+<meta name="author" content="WrapTheme, design by: ThemeMakker.com">
+
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+<!-- VENDOR CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/1.0.0/css/simple-line-icons.min.css" integrity="sha512-MsZfOEN3I+laIjgQzZOFqXoEXIOW8T7EXo/RhNedrtBYMXZvr7dDq8QN4Fw0B3ocQU1ge7auTf0dCpT9X3U9cg==" crossorigin="anonymous" />
+<link rel="stylesheet" href="{{asset('assets/backend/vendor/bootstrap/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/backend/vendor/font-awesome/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/backend/vendor/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/backend/vendor/jvectormap/jquery-jvectormap-2.0.3.min.css')}}"/>
+<link rel="stylesheet" href="{{asset('assets/backend/vendor/morrisjs/morris.min.css')}}" />
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="{{asset('assets/backend/css/main.css')}}">
+<link rel="stylesheet" href="{{asset('assets/backend/css/color_skins.css')}}">

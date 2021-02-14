54f51befcef47d9a898d01ad0069172bcc90308a
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-02-15 01:30:47 +0600

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(10)->create();
     }
 }
